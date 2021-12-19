6cd6a7e6024aa1ad64f1554903fdcd4f33c7b48b
Krazune <krazune@gmail.com>
2021-12-19 01:38:44 +0000

    Restructure objects group

--- src/main/java/com/krazune/hiddendropparty/KObject.java
+++ src/main/java/com/krazune/hiddendropparty/KObject.java
@@ -1,5 +1,7 @@
 package com.krazune.hiddendropparty;
 
+import java.util.ArrayList;
+import java.util.List;
 import net.runelite.api.Client;
 import net.runelite.api.Model;
 import net.runelite.api.RuneLiteObject;
@@ -9,18 +11,32 @@ import net.runelite.api.coords.WorldPoint;
 // I swear I'm not narcissistic, I just didn't know what to name this class. Might change in the future.
 public class KObject
 {
-	private final WorldPoint location;
-	private final int modelId;
-
 	private final Client client;
 
-	private RuneLiteObject object;
+	private final WorldPoint location;
+	private final List<Integer> modelIds;
+	private List<RuneLiteObject> objects;
 
-	public KObject(Client client, WorldPoint location, int modelId)
+	public KObject(Client client, WorldPoint location, int... modelIds)
 	{
 		this.client = client;
 		this.location = location;
-		this.modelId = modelId;
+		this.modelIds = new ArrayList<>();
+
+		for (int i = 0; i < modelIds.length; ++i)
+		{
+			this.modelIds.add(modelIds[i]);
+		}
+
+		objects = new ArrayList<>();
+	}
+
+	public KObject(Client client, WorldPoint location, List<Integer> modelIds)
+	{
+		this.client = client;
+		this.location = location;
+		this.modelIds = modelIds;
+		objects = new ArrayList<>();
 	}
 
 	public WorldPoint getLocation()
@@ -28,51 +44,48 @@ public class KObject
 		return location;
 	}
 
-	public int getModelId()
+	public List<Integer> getModelIds()
 	{
-		return modelId;
+		return modelIds;
 	}
 
 	public void spawn()
 	{
-		if (object != null)
+		despawn();
+
+		for (int i = 0; i < modelIds.size(); ++i)
 		{
-			object.setActive(false);
+			LocalPoint localLocation = LocalPoint.fromWorld(client, location);
+
+			if (localLocation == null)
+			{
+				continue;
+			}
+
+			Model newModel = client.loadModel(modelIds.get(i));
+
+			if (newModel == null)
+			{
+				continue;
+			}
+
+			RuneLiteObject newObject = client.createRuneLiteObject();
+
+			newObject.setLocation(localLocation, location.getPlane());
+			newObject.setModel(newModel);
+			newObject.setActive(true);
+
+			objects.add(newObject);
 		}
-
-		LocalPoint localLocation = LocalPoint.fromWorld(client, location);
-
-		if (localLocation == null)
-		{
-			return;
-		}
-
-		RuneLiteObject newObject = client.createRuneLiteObject();
-		Model newModel = loadModel();
-
-		if (newModel == null)
-		{
-			return;
-		}
-
-		newObject.setLocation(localLocation, location.getPlane());
-		newObject.setModel(newModel);
-		newObject.setActive(true);
 	}
 
 	public void despawn()
 	{
-		if (object == null)
+		for (int i = 0; i < objects.size(); ++i)
 		{
-			return;
+			objects.get(i).setActive(false);
 		}
 
-		object.setActive(false);
-		object = null;
-	}
-
-	private Model loadModel()
-	{
-		return client.loadModel(modelId);
+		objects.clear();
 	}
 }

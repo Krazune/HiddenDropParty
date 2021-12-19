d175ee081d6c85644c94ca27b0be4640c02b9ea6
Krazune <krazune@gmail.com>
2021-12-19 00:36:16 +0000

    Add object group

--- src/main/java/com/krazune/hiddendropparty/HiddenDropPartyPlugin.java
+++ src/main/java/com/krazune/hiddendropparty/HiddenDropPartyPlugin.java
@@ -1,5 +1,9 @@
 package com.krazune.hiddendropparty;
 
+import com.google.inject.Inject;
+import net.runelite.api.Client;
+import net.runelite.api.events.ItemSpawned;
+import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
@@ -18,4 +22,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 )
 public class HiddenDropPartyPlugin extends Plugin
 {
+	@Inject
+	private Client client;
+
+	@Subscribe
+	public void onItemSpawned(ItemSpawned itemSpawned)
+	{
+		KObject base = new KObject(client, itemSpawned.getTile().getWorldLocation(), 40205);
+		KObject chest = new KObject(client, itemSpawned.getTile().getWorldLocation(), 11123);
+
+		new KObjectGroup(base, chest).spawn();
+	}
 }

c4b011e40d67e7e6268a62e72864a3e2b2329639
Krazune <krazune@gmail.com>
2021-12-20 14:54:05 +0000

    Add basic functionality

--- src/main/java/com/krazune/hiddendropparty/HiddenDropPartyPlugin.java
+++ src/main/java/com/krazune/hiddendropparty/HiddenDropPartyPlugin.java
@@ -1,5 +1,13 @@
 package com.krazune.hiddendropparty;
 
+import com.google.inject.Inject;
+import net.runelite.api.Client;
+import net.runelite.api.GameState;
+import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.ItemDespawned;
+import net.runelite.api.events.ItemSpawned;
+import net.runelite.client.callback.ClientThread;
+import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
@@ -18,4 +26,62 @@ import net.runelite.client.plugins.PluginDescriptor;
 )
 public class HiddenDropPartyPlugin extends Plugin
 {
+	@Inject
+	private Client client;
+
+	@Inject
+	private ClientThread clientThread;
+
+	private KObjectLocationRegistry registry;
+
+	@Subscribe
+	public void onItemSpawned(ItemSpawned itemSpawned)
+	{
+		registry.add(itemSpawned.getTile().getWorldLocation());
+	}
+
+	@Subscribe
+	public void onItemDespawned(ItemDespawned itemDespawned)
+	{
+		registry.remove(itemDespawned.getTile().getWorldLocation());
+	}
+
+	@Subscribe
+	public void onGameStateChanged(GameStateChanged gameStateChanged)
+	{
+		GameState newState = gameStateChanged.getGameState();
+
+		if (newState == GameState.LOGIN_SCREEN || newState == GameState.HOPPING)
+		{
+			clientThread.invokeLater(this::resetRegistry);
+		}
+		else if (newState == GameState.LOADING)
+		{
+			registry.reset();
+		}
+	}
+
+	@Override
+	protected void startUp()
+	{
+		registry = new KObjectLocationRegistry(client);
+	}
+
+	@Override
+	protected void shutDown()
+	{
+		clientThread.invokeLater(this::deleteRegistry);
+	}
+
+	private void resetRegistry()
+	{
+		registry.despawnAll();
+		registry = new KObjectLocationRegistry(client);
+	}
+
+	private void deleteRegistry()
+	{
+		registry.despawnAll();
+		registry = null;
+	}
 }

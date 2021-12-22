ea258585cf33622ac6a5c584a63b16a2ebe253ec
Krazune <krazune@gmail.com>
2021-12-22 19:11:49 +0000

    Rename crates group

--- src/main/java/com/krazune/hiddendropparty/ChestModelIdsGroup.java
+++ src/main/java/com/krazune/hiddendropparty/ChestModelIdsGroup.java
@@ -30,7 +30,7 @@ package com.krazune.hiddendropparty;
 public enum ChestModelIdsGroup
 {
 	DEFAULT("11123,15567,15885"),
-	WOODEN_CRATES("12152,15509,29973,33922,31450"),
+	CRATES("12152,15509,29973,33922,31450"),
 	CHESTS("11123,11204,12146,12150,12530"),
 	CUSTOM("");
 
